010a8b15b5c20cf785e4ae3cfdcbebb94f63dce6
Erling Paulsen <erling.paulsen@aenergi.no>
2025-10-18 18:05:29 +0200

    Fix

--- src/NetDaemon/Program.cs
+++ src/NetDaemon/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using HomeAutomations.Apps;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NetDaemon.AppModel;
 using NetDaemon.Extensions.Logging;
+using NetDaemon.Extensions.MqttEntityManager;
 using NetDaemon.Runtime;
 using Serilog;
 
@@ -8,10 +12,16 @@ try
     await Host.CreateDefaultBuilder(args)
         .UseNetDaemonAppSettings()
         .UseNetDaemonRuntime()
+        .UseNetDaemonMqttEntityManagement()
+        .ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddJsonFile("appsettings.Development.json", optional: true);
+        })
         .ConfigureServices((_, services) =>
         {
             services
-                .AddNetDaemonStateManager();
+                .AddNetDaemonStateManager()
+                .AddNetDaemonApp<CostSensorApp>();
         })
         .UseSerilog((context, configuration) =>
         {
